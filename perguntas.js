f9bb6ccdbdd3cd761379b12126b1c4fe6cf064c6
leandropbl <00001113449159sp@al.educacao.sp.gov.br>
2024-11-22 15:56:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+criaCartao(
+    'Geografia', 
+    'Qual a capital do Ucrania?',
+    'Kiev'
+)
+
+criaCartao(
+    'Historia', 
+    'Qual foi a operação ousada que hitler fez no ano de 1941?',
+    'Operação Barbarossa'
+)
+
+criaCartao(
+    'Biologia', 
+    'Qual e o maior orgão do corpo humano?',
+    'A pele Humana'
+)
+
+criaCartao(
+    'Qual é a maior nação do War Thunder?',
+    'A nação dos EUA ou Estados Unidos'
+)
+
+criaCartao(
+    'Quais são as melhores nações do War Thunder?',
+    'Quais são os melhores tiers?',
+    'America: Tanques Medios estabilizados, Alemanha: Tanques Pesados e bem angulados e União Sovietica: Tanques com alto poder de fogo e perfuração.'
+)
+
+criaCartao(
+    'Quais as nações mais complexas?',
+    'Nação Italiana, Francesa e chinesa'
+)
+criaCartao(
+    'Qual é a pior nação do War Thunder?',
+    'A nação dos Italianos'
+)
+
+criaCartao(
+    'Quais são as nações medianas do War Thunder?',
+    'A nação Inglesa e japonesa.'
+)
+
+criaCartao(
+    'Qual  a melhor e mais facil nação do War Thunder?',
+    'Nação da Alemanha'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
